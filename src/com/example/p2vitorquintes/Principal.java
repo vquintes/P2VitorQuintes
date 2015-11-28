739b901d0e99e78e68b101cb384a2812fd4bf7ca
Vitor Quintes <vitorquintes@yahoo.com.br>
2015-11-28 13:47:19 -0200

    Acertos nos Métodos OnCreate das Classes calendario.java e cronometro.java.

--- src/com/example/p2vitorquintes/Principal.java
+++ src/com/example/p2vitorquintes/Principal.java
@@ -24,8 +24,11 @@ public class Principal extends Activity {
 		button1 = (Button)
 		findViewById (R.id.button1);
 		
+		
+			
 		button2 = (Button)
 		findViewById (R.id.button2);
+		
 		
 		button1.setOnClickListener(new View.OnClickListener() {
 			
@@ -54,20 +57,17 @@ public class Principal extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.principal, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
+		
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
-}
+	}
--- src/com/example/p2vitorquintes/cronometro.java
+++ src/com/example/p2vitorquintes/cronometro.java
@@ -10,7 +10,7 @@ public class cronometro extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_principal);
+		setContentView(R.layout.calendario);
 	}
 
 	@Override
